27b0e3bbbf613e448811c012741975d40b1c0c84
Povilas <SKIRIUSPOVILAS@GMAIL.COM>
2024-05-15 14:29:12 +0300

    sum positives cw

--- js/cw/cw1.js
+++ js/cw/cw1.js
@@ -94,3 +94,18 @@ function addLength2(str) {
 }
 console.log(addLength('apple ban'));
 console.log(addLength2('you will win'));
+
+console.clear()
+//sum positives
+function positiveSum(arr) {
+    let results = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            results += arr[i];
+        }
+    }
+    return results;
+}
+
+console.log(positiveSum([1, -2, 3, 4, 5]))
+console.log(positiveSum([]))

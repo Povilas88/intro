a377d7caf0ff077ca67bc37b917fefe6ee1a8097
Povilas <SKIRIUSPOVILAS@GMAIL.COM>
2024-05-28 14:46:06 +0300

    cw5

--- js/cw/cw5.js
+++ js/cw/cw5.js
@@ -61,3 +61,68 @@ console.log(isOpposite("aBcd", "AbCD"), true)
 console.log(isOpposite("aBcde", "AbCD"), false)
 console.log(isOpposite("AB", "Ab"), false)
 console.log(isOpposite("", ""), false)
+
+//Replace With Alphabet Position
+console.log('\nReplace With Alphabet Position\n')
+function alphabetPosition(text) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const split = alphabet.split('');
+    const low = text.toLowerCase().split('');
+    let result = [];
+
+    for (let i = 0; i < low.length; i++) {
+        for (let j = 0; j < split.length; j++) {
+            if (low[i] === split[j]) {
+                result.push(j + 1);
+            }
+        }
+    }
+    return result.join(' ');
+}
+console.log(alphabetPosition("The sunset sets at twelve o' clock."))
+
+//Persistent Bugger.
+console.log('\nPersistent Bugger.\n');
+function persistence(num) {
+    let result = 0;
+    let numStr = num.toString();
+
+    if (numStr.length === 1) {
+        return 0;
+    }
+    while (numStr.length > 1) {
+        num = numStr.split('').reduce((a, b) => a * parseInt(b), 1);
+        numStr = num.toString();
+        result++
+    }
+    return result;
+}
+
+console.log(persistence(39));
+
+//Convert string to camel case
+console.log('\nConvert string to camel case\n');
+function toCamelCase(str) {
+    const split1 = str.split('_').join(' ');
+    const split2 = split1.split('-').join(' ');
+    const split3 = split2.split(' ');
+
+    if (str === '') {
+        return '';
+    }
+    for (let i = 0; i < split3.length; i++) {
+        split3[i] = split3[i].charAt(0).toUpperCase() + split3[i].slice(1);// replace first character
+        if (split3[0].charAt(0) !== str.charAt(0)) {
+            split3[0] = split3[0].charAt(0).toLowerCase() + split3[0].slice(1);
+        }
+    }
+    return split3.join('')
+}
+
+console.log(toCamelCase("the_stealth_warrior"));
+
+/*
+function toCamelCase(str){
+  return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
+}
+*/
